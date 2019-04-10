73830dfbc689022e1f77ca8df9632f0a5093e5b8
Jamaludin Osmanov <pr3gt4u@gmail.com>
2019-04-10 11:53:30 +0300

    initial commit

---
+++ script.ts
@@ -0,0 +1,29 @@
+import $ = require("jquery");
+
+class StyleSheets {
+
+    public addStyleSheetRules = (rules, styleId = 'gt4u_styles') => {
+
+        let $style = $(`#${styleId}`)[0] || $(`<style id="${styleId}"></style>`).appendTo('head')[0];
+
+        let styleSheet = $style.sheet;
+
+        $.each(rules, (selector, styles) => {
+
+            let propStr = '';
+
+            $.each(styles, (property, style) => {
+
+                propStr += `${property}: ${style};\n`;
+
+            });
+
+            styleSheet.insertRule(`${selector} {${propStr}}`, styleSheet.cssRules.length);
+
+        });
+
+        return styleSheet;
+
+    }
+
+}
